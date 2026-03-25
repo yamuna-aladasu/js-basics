271ec726c3ec1a20f2624fafc41ba70ec21aaa8a
yamuna aladasu <yamuna.aladasu@bigthinkcode.com>
2026-03-25 14:35:59 +0530

    stringmethods

---
+++ stringmet.js
@@ -0,0 +1,20 @@
+let str = "Hello world";
+console.log(str.length);
+console.log(str.charAt(2));
+console.log(str.includes("H"));
+console.log(str.indexOf("l"));
+console.log(str.lastIndexOf("l"));
+console.log(str.startsWith("H"));
+console.log(str.endsWith("d"));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.concat(" ","hello"));
+console.log(str.slice(0,4));
+console.log(str.split(" "));
+console.log(str.repeat(3));
+console.log(str.replace("He","ll"));
+
+console.log(str.trim());
+console.log(str.trimStart());
+console.log(str.trimEnd());
+
